7c6b1410b083585a2c141ff1fae3017e731302c6
hussein <hussein66253@icloud.com>
2020-02-20 13:37:41 +0200

    added SumAndMultiply

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(c, d) { //eslint-disable-line
-        var theMultiply = c*d; /*  5*9=45 */
-        var message = `The product of ${c} and ${d} is ${theMultiply}.`;
+function multiply(a, b) { //eslint-disable-line
+        var theMultiply = a*b; /*  5*9=45 */
+        var message = `The product of ${a} and ${b} is ${theMultiply}.`;
         return[theMultiply, message];
         }     
 // Here is the test for multiply(); uncomment it to run it
@@ -50,14 +50,30 @@ Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+// if (sumAndMultiply(4, 7, 5)[0] === 16 && sumAndMultiply(4, 7, 5)[1] === 140 && sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(a, b,c) { //eslint-disable-line
+    var sum2 = sum(a,b) ;
+    var sum1 = sum(sum2[0],c);
+    // console.log(sum1[0])
+
+    var message = `${a} and ${b} and ${c} sum to ${sum1[0]}.`;
+    // console.log(message)
+    var multiply2 = multiply(a ,b);
+    var multiply3 = multiply(multiply2[0],c);
+    // console.log(multiply3[0])
+
+    var message1 = `The product of ${a} and ${b} and ${c} is ${multiply3[0]}.`;
+    // console.log(message1)
+
+    return [ sum1[0], multiply3[0], message, message1];
+    // return [`${a} and ${b} and ${c} sum to ${multiply3}.`]
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
