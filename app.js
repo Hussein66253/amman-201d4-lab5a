eddf8cacc6e1bf18eaea9b2a29e73c791298b058
hussein <hussein66253@icloud.com>
2020-02-20 11:58:55 +0200

    sum added

--- app.js
+++ app.js
@@ -30,9 +30,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
 
-}
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
